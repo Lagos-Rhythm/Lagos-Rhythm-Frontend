ebf795650318f5aeed15b658551e8933cf4784c4
chiscookeke11 <chiscookeke11@gmail.com>
2026-03-09 14:35:17 +0100

    added the about us scaffold page and the component

---
+++ app/(website)/about/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next"
+
+
+
+
+
+export const metadata: Metadata = {
+    title: "About Us | Lagos Rhythm",
+    description: ""
+}
+
+export default function Page() {
+    return (
+        <div>
+            About us
+        </div>
+    )
+}
